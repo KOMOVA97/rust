271d45e59498a7b2cf7ec57438bd954888f2f148
komova97 <513335949@qq.com>
2024-02-01 21:10:52 +0800

    添加test文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod test;
+
 use num::Complex;
 use std::str::FromStr;
 
@@ -108,38 +110,3 @@ fn test_pixel_to_point() {
                Complex { re: -0.5, im: -0.75 });
 }
 
-// 平民（commoner）们见多识广，收到什么礼物都能应对。
-// 所有礼物都显式地使用 `match` 来处理。
-fn give_commoner(gift: Option<&str>) {
-    // 指出每种情况下的做法。
-    match gift {
-        Some("snake") => println!("Yuck! I'm throwing that snake in a fire."),
-        Some(gift)   => println!("{}? How nice.", gift),
-        None          => println!("No gift? Oh well."),
-    }
-}
-
-fn give_princess(gift: Option<&str>) {
-    // `unwrap` 在接收到 `None` 时将返回 `panic`。
-    let inside = gift.unwrap();
-    if inside == "snake" { panic!("AAAaaaaa!!!!"); }
-
-    println!("I love {}s!!!!!", inside);
-}
-
-#[test]
-fn test_give_commer() {
-    let food  = Some("chicken");
-    let snake = Some("snake");
-    let void  = None;
-
-    give_commoner(food);
-    give_commoner(snake);
-    give_commoner(void);
-
-    let bird = Some("robin");
-
-
-    give_princess(bird);
-    //give_princess(nothing);
-}
---
+++ src/test.rs
@@ -0,0 +1,36 @@
+// 平民（commoner）们见多识广，收到什么礼物都能应对。
+// 所有礼物都显式地使用 `match` 来处理。
+fn give_commoner(gift: Option<&str>) {
+    // 指出每种情况下的做法。
+    match gift {
+        Some("snake") => println!("Yuck! I'm throwing that snake in a fire."),
+        Some(gift)   => println!("{}? How nice.", gift),
+        None          => println!("No gift? Oh well."),
+    }
+}
+
+fn give_princess(gift: Option<&str>) {
+    // `unwrap` 在接收到 `None` 时将返回 `panic`。
+    let inside = gift.unwrap();
+    if inside == "snake" { panic!("AAAaaaaa!!!!"); }
+
+    println!("I love {}s!!!!!", inside);
+}
+
+#[test]
+fn test_give_commer() {
+    let food  = Some("chicken");
+    let snake = Some("snake");
+    let void  = None;
+
+    give_commoner(food);
+    give_commoner(snake);
+    give_commoner(void);
+
+    let bird = Some("robin");
+
+
+    println!("ceshi{:?}", food);
+    give_princess(bird);
+    //give_princess(nothing);
+}
